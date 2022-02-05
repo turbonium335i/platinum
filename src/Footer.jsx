e27a1d3a36587d8b3aea5ae95e2e760a31f7ef5b
unknown <turbonium335i@gmail.com>
2022-02-05 23:36:12 +0900

    godaddy update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,19 +1,20 @@
-const Footer = () => <footer className="page-footer font-small blue pt-4">
+const Footer = () => (
+  <footer className="page-footer font-small blue pt-4">
     <div className="container-fluid text-center text-md-left bg-dark text-light pt-2">
-        <div className="row">
-            <div className="col mt-md-0 mt-3">
-               
-                <p>Copyright © 2022 &nbsp;  
-    <a href={'http://pertinacity1.pythonanywhere.com/landing'}>Developed and Managed by www.evolution3w.com</a></p>
-            </div>
-
-            <hr className="clearfix w-100 d-md-none pb-0"/>
-
-
+      <div className="row">
+        <div className="col mt-md-0 mt-3">
+          <p>
+            Copyright © 2022 &nbsp;
+            <a href={"https://evolution3w.com"}>
+              Developed and Managed by www.evolution3w.com
+            </a>
+          </p>
         </div>
+
+        <hr className="clearfix w-100 d-md-none pb-0" />
+      </div>
     </div>
- 
+  </footer>
+);
 
-</footer>
-
-export default Footer
+export default Footer;
